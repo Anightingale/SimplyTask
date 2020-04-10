5fa364b5a29b235bc2790e46a015b3722b400a75
anightingale <anigh3@hotmail.com>
2020-04-10 23:38:18 +1000

    All page skeletons complete - Home Page skeleton added

--- app/src/main/java/com/example/simplytask/Maths.java
+++ app/src/main/java/com/example/simplytask/Maths.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.simplytask.R;
-
 public class Maths extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.subjectOverview);
     }
 
 }

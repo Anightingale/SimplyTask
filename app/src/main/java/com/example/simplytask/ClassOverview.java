7debb3fd41ee00c382adf76671c98dbab29eb388
anightingale <anigh3@hotmail.com>
2020-04-11 06:23:03 +1000

    logoooooooooooooooooo

---
+++ app/src/main/java/com/example/simplytask/ClassOverview.java
@@ -0,0 +1,14 @@
+package com.example.simplytask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_overview);
+    }
+}
